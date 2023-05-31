7b149e1b4c7f2b8fe49103f3c51ad55952dd0b21
fyzce <125534091+fyzce@users.noreply.github.com>
2023-05-31 12:06:38 -0500

    Create Configurations.lua

---
+++ Source/Configurations.lua
@@ -0,0 +1,8 @@
+return {
+	PLUGIN_NAME = "Insert",
+	PLUGIN_DESC = "Insert everything necessary for Knit in one click!",
+	PLUGIN_ICON = "rbxassetid://13233829028",
+	PLUGIN_LOGOMARK = "rbxassetid://13233866058",
+	ERROR_LIMIT = 1,
+	ERROR_MAX = 5,
+}
