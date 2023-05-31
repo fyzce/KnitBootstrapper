d5301edc0145644349747085f3e4dfc8c6570678
fyzce <125534091+fyzce@users.noreply.github.com>
2023-05-31 12:05:35 -0500

    Delete Configurations.lua

--- Configurations.lua
+++
@@ -1,8 +0,0 @@
-return {
-	PLUGIN_NAME = "Insert",
-	PLUGIN_DESC = "Insert everything necessary for Knit in one click!",
-	PLUGIN_ICON = "rbxassetid://13233829028",
-	PLUGIN_LOGOMARK = "rbxassetid://13233866058",
-	ERROR_LIMIT = 1,
-	ERROR_MAX = 5,
-}
